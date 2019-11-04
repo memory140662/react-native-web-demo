a58411ae90f36a70b267f2267664df02828504bc
Leo <memory140662@gmail.com>
2019-11-04 16:40:47 +0800

    add data list and search bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  View,
+  FlatList
+} from 'react-native'
+import RW from 'random-words'
+import {
+  ListItem,
+  SearchBar
+} from 'react-native-elements'
+
+const list = []
+for (let i = 0; i < 20; i++) {
+  list.push({
+    title: RW(),
+    subtitle: RW(),
+    key: i + '',
+  })
 }
 
-export default App;
+function App() {
+  const [ search, setSearch ] = useState('')
+  return (
+    <View>
+      <style type="text/css">{`
+        @font-face {
+          font-family: 'MaterialIcons';
+          src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+        }
+
+        @font-face {
+          font-family: 'FontAwesome';
+          src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
+        }
+      `}</style>
+      <SearchBar 
+        placeholder="Search Text..."
+        onChangeText={text => setSearch(text)}
+        value={search}
+      />
+      <FlatList 
+        data={list.filter(data => data.title.indexOf(search) !== -1)}
+        renderItem={({item}) => <ListItem 
+          key={item.key} 
+          {...item}
+          bottomDivider
+          chevron
+        />}
+      />
+    </View> 
+  )
+}
+
+export default App
